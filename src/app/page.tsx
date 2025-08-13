56b52a2a2327f166c7860921acd3c8fa35233582
Felipe Cacezi <felipecacezi@gmail.com>
2025-08-13 23:58:15 +0000

    a top bar e o rodapé podem encostar nas laterais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 container mx-auto px-4">
         <Hero />
         <Features />
         <Contact />
