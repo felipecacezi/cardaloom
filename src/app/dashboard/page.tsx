257428657731a382d20201587194d52ddbb828c7
Felipe Cacezi <felipecacezi@gmail.com>
2025-08-19 23:39:56 +0000

    na pagina inicial deixe somente a mensagem bem vindo ao seu painel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -61,7 +61,6 @@ export default function DashboardPage() {
         <div className="border-2 border-dashed border-muted rounded-lg h-full p-8 flex items-center justify-center">
           <div className='text-center'>
                <h2 className="text-2xl font-bold mb-4">Bem-vindo ao seu Painel!</h2>
-              <p className="text-muted-foreground">Aqui você poderá gerenciar seu cardápio, visualizar estatísticas e muito mais.</p>
           </div>
         </div>
       </main>
